ca8e0164c6a4a29b806730264ea698abb315c195
JUAN DAVID Quinones <juandavid9812@gmail.com>
2024-11-07 22:53:31 -0500

    Agregar archivos de proyecto.

---
+++ Models/Provider.cs
@@ -0,0 +1,12 @@
+﻿namespace SupermarketWEB.Models
+{
+    public class Provider
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+		public int ProductId { get; set; }
+		public Product? Product { get; set; } = default!;
+    }
+}
