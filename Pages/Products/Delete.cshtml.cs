0a8c6b10f1bfca5fb3173bc7520d06e69ba5bead
JUAN DAVID Quinones <juandavid9812@gmail.com>
2024-11-07 23:26:33 -0500

    Delete Products

--- Pages/Products/Delete.cshtml.cs
+++ Pages/Products/Delete.cshtml.cs
@@ -52,7 +52,7 @@ namespace SupermarketWEB.Pages.Products
 				Product = product;
 				_context.Products.Remove(Product);
 				await _context.SaveChangesAsync();
-
+				{ }
 			}
 			return RedirectToPage("./Index");
 		}
