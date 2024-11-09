eec3db481d17e9a27e5e623022b33371a7af6c7b
JUAN DAVID Quinones <juandavid9812@gmail.com>
2024-11-09 10:26:02 -0500

    Login y Logout

--- Pages/Account/Logout.cshtml.cs
+++ Pages/Account/Logout.cshtml.cs
@@ -4,8 +4,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SupermarketWEB.Pages.Account
 {
-    public class LogoutModel : PageModel
-    {
+	public class LogoutModel : PageModel
+	{
 		public async Task<IActionResult> OnPostAsync()
 		{
 			await HttpContext.SignOutAsync("MyCookieAuth");
